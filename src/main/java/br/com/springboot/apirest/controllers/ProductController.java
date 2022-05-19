0f8a8f6c61e35325086a39efc920e78587c00895
Brendson <brendson.net@gmail.com>
2022-05-19 19:56:30 -0300

    feat: created route delete product

--- src/main/java/br/com/springboot/apirest/controllers/ProductController.java
+++ src/main/java/br/com/springboot/apirest/controllers/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +33,12 @@ public class ProductController {
   }
 
   @GetMapping("/product/{id}")
-  public Optional<Product> readId(@PathVariable(value="id") Long id){
+  public Optional<Product> readId(@PathVariable(value = "id") Long id){
     return this.productRepository.findById(id);
+  }
+
+  @DeleteMapping("/product/{id}")
+  public void delete(@PathVariable(value = "id") Long id){
+    this.productRepository.deleteById(id);
   }
 }
